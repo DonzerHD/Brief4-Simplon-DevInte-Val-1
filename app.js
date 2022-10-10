16c317137b893fca870721d40ae2a67fb104d02a
Thomas.l59 <thomas.lemay59@outlook.fr>
2022-10-10 07:40:38 +0200

    js mi

---
+++ app.js
@@ -0,0 +1,13 @@
+
+const menuHamburger = document.querySelector(".toggle")
+ const navLinks = document.querySelector(".menu")
+ const Kebab = document.querySelector(".chef")
+ 
+menuHamburger.addEventListener('click',()=>{
+ navLinks.classList.toggle('mobile-menu') }
+ )
+
+ menuHamburger.addEventListener('click',()=>{
+    Kebab.classList.toggle('dur') }
+)
+   
